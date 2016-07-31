f86c13a1f8ecddf25a3df1fce05687258d07fb4b
facelessuser <faceless.shop@gmail.com>
2016-07-31 11:07:27 -0600

    Add changeling command

---
+++ CHANGES.md
@@ -0,0 +1,12 @@
+# ScopeHunter 2.5.0
+> Released Jul 31, 2016
+
+- **NEW**: Changelog command available in `Package Settings->ScopeHunter`.  
+Will render a full changlog in an HTML phantom in a new view.
+- **NEW**: Support info command available in `Package Settings->ScopeHunter`.
+- **NEW**: Will attempt to tell Package Control to update the most recent  
+desired mdpopups.  Really need to test that this actually does works :).
+- **NEW**: Requires `mdpopups` version 1.9.0.  Run Package Control  
+`Satisfy Dependencies` command if not already present. May require restart after  
+update.
+- **FIX**: Formatting fixes.
---
+++ changes.py
@@ -0,0 +1,31 @@
+"""Changelog."""
+import sublime
+import sublime_plugin
+import mdpopups
+
+CSS = '''
+.scope-hunter h1, .scope-hunter h2, .scope-hunter h3, .scope-hunter h4, .scope-hunter h5, .scope-hunter h6 {
+    {{'.string'|css('color')}}
+}
+.scope-hunter blockquote { {{'.comment'|css('color')}} }
+'''
+
+class ScopeHunterChangesCommand(sublime_plugin.WindowCommand):
+    """Changelog command."""
+
+    def run(self):
+        """Show the changelog in a new view."""
+        text = sublime.load_resource('Packages/ScopeHunter/CHANGES.md')
+        view = self.window.new_file()
+        view.set_name('ScopeHunter - Changlog')
+        view.settings().set('gutter', False)
+        html = '<div class="scope-hunter">%s</div>' % mdpopups.md2html(view, text)
+        mdpopups.add_phantom(view, 'chagelog', sublime.Region(0), html, sublime.LAYOUT_INLINE, css=CSS)
+        view.set_read_only(True)
+        view.set_scratch(True)
+
+    def is_enabled(self):
+        """Check if is enabled."""
+        return (mdpopups.version() >= (1, 7, 3)) and (int(sublime.version()) >= 3118)
+
+    is_visible = is_enabled
